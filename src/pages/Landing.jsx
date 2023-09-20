ba89b0a43461313c962410caeb03cacc60e21d2a
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-09-20 13:27:48 +0200

    add landing page

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,15 @@
+// import aluveLogo from '../assets/aluve_monochrome_black.png';
+
+import '../index.css';
+
+export default function Landing() {
+   
+    return (
+        <>
+            <div className="relative">
+                <p>Landing</p>
+
+            </div>
+        </>
+    );
+}
