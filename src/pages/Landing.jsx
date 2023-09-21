5e12ab276017b8eaedc6ea70e686f262dbaa4b2b
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-09-21 22:48:11 +0200

    :lipstick: Add tablet breakpoint
    add tablet breakpoint styling

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,38 +5,40 @@ import '../index.css';
 export default function Landing() {
    
     return (
-        <>
-            <div className="relative h-screen overflow-hidden">
-                <div className="display flex flex-col bg-white justify-center w-full h-screen sm:flex-row ">
-                    <div className='bg-lilac w-full flex-grow py-24 pl-32 pr-80 text-left text-white'>
-                        <div className=''>
-                        <p className='text-6xl font-bold  '>
-                            Load shedding impacting your  <br/> team’s<br/> productivity?
-                        </p>
+        
+  <>
+  <div className="relative min-h-screen lg:overflow-hidden overflow-visible justify-center">
+      <div className="display flex flex-col bg-white justify-center w-full min-h-screen sm:flex-row flex-grow">
+          <div className='bg-lilac w-full md:pr-48  flex-grow pt-24 pl-32 pr-80 text-left lg:text-white md:text-white sm:text-black'>
+              <div className='flex-grow'>
+              <p className='lg:text-6xl font-bold text-4xl md:text-5xl flex-grow'>
+                  Load shedding impacting your  <br/> team’s<br/> productivity?
+              </p>
 
-                        <p className='text-2xl pt-16 pr-48 leading-none'>
-                            Find the best time to collaborate with your team. Schedule a meeting. Send messages, plus more. <br/>Ready?
-                        </p>
+              <p className='text-2xl pt-16 pr-16 leading-none '>
+                  Find the best time to collaborate with your team. Schedule a meeting. Send messages, plus more. <br/>Ready?
+              </p>
 
-                        <div className='display flex row justify-space-around w-full gap-6 text-2xl pt-16'>
-                            <button className='px-10 py-2 rounded bg-dark-blue'>Sign In</button>
-                            <button className='px-10 py-2 rounded bg-light-blue'>Register</button>
-                        </div>
-                        </div>
-                        
-                        <div className='relative -top-3/4  left-3/4  inset-1/4'>
-                           <img src={ timeImgBlack } width={"700px"}  alt="time image black" /> 
-                        </div>
-                        
-                    </div>
+              <div className='display flex row justify-space-around w-full gap-6 lg:text-2xl sm:text-xl pt-16'>
+                  <button className='px-10 py-2 rounded bg-dark-blue'>Sign In</button>
+                  <button className='px-10 py-2 rounded bg-light-blue'>Register</button>
+              </div>
+              </div>
+              
+              <div className='relative -top-96 inset-full '>
+                 <img src={ timeImgBlack } width={"800px"}  alt="time image black" /> 
+              </div>
+              
+          </div>
 
-                    {/* FIX: top and bottom left border radius needs to be applied */}
-                    <div className='bg-white w-full max-w-md sm:w-1/2 flex-grow rounded-border'>
-                        <img src={ aluveLogo } width={"300px"}  alt="aluve logo" />
-                    </div>
-                </div>
+          {/* FIX: top and bottom left border radius needs to be applied */}
+          <div className='bg-white w-full max-w-md sm:w-1/2 flex-grow rounded-border'>
+              <img src={ aluveLogo } width={"300px"}  alt="aluve logo" />
+          </div>
+      </div>
 
-            </div>
-        </>
+  </div>
+</>
+      
     );
 }
