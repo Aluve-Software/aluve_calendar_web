c8e2a41c21647aa1ac240574f9ee7543093593c1
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-09-24 01:06:46 +0200

    :lipstick: Update input styling
    update input styling

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -37,10 +37,10 @@ export default function Registration() {
                     </div>
                     
 
-                    <div className='bg-white w-full pr-6 pl-6 md:pr-24 md:pl-20 lg:w-1/4 md:w-1/4 flex-grow lg:py-16 md:py-16 lg:px-16 md:px-16 lg:pt-24 md:pt-24 lg:pl-32 lg:pr-80 text-left lg:text-white md:text-white sm:text-black '>
+                    <div className='bg-white w-full pr-6 pl-6 md:pr-24 md:pl-20 lg:w-1/4 md:w-1/4 flex-grow lg:py-16 md:py-16 lg:px-60 md:px-16 lg:pt-8 md:pt-24  text-left lg:text-white md:text-white sm:text-black '>
                         <div className='flex-grow flex flex-col items-center justify-center'>
-                            <div className='hidden lg:block md:block text-grey'>
-                                <p className=' font-bold text-2xl lg:text:2xl'>Create your account</p>
+                            <div className='hidden lg:block md:block text-grey '>
+                                <p className=' font-bold text-2xl lg:text:2xl pb-10'>Create your account</p>
 
                                 {/* LINK to auth functionality */}
                                 {/* ADD input and onChange triggers */}
@@ -56,18 +56,18 @@ export default function Registration() {
                                     <span className='text-xs'>* Your password must be at least 8 characters including a number, a symbol, a lowercase and uppercase letter.</span>
                                 </div> 
 
-                                <div className='text-sm pb-5'>
+                                <div className='text-sm '>
                                     <label htmlFor="password" className=''>Confirm Password</label>
                                     <input type="password" id="password" className="bg-white border-2 border-lilac rounded-md text-light-grey w-full p-2.5" placeholder="Enter password again" required/>
                                 </div> 
                             </div>  
 
                             {/* Link buttons to actions and pages */}
-                            <div className='flex flex-col items-center gap-4  justify-center w-full lg:w-3/4  lg:text-md text-sm md:text-md font-bold py-4 lg:pt-16 md:pt-8'>
-                                <button className='w-full lg:px-10 py-2 md:px-5 rounded-lg md:rounded lg:rounded bg-light-blue text-white'>Register With Email</button>
+                            <div className='flex flex-col items-center gap-4  justify-center w-full  lg:text-md text-sm md:text-md font-bold py-4 lg:pt-16 md:pt-8'>
+                                <button className='w-full lg:px-10 py-2 lg:py-3 md:py-3 md:px-5 rounded-lg md:rounded lg:rounded bg-light-blue text-white'>Register With Email</button>
                                 <p className='text-grey'>OR</p>
-                                <button className='w-full lg:px-10 py-2 md:px-5 rounded-lg md:rounded lg:rounded border-2 border-bright-orange text-bright-orange'>Register With Google</button>
-                                <button className='w-full lg:px-10 py-2 md:px-5 rounded-lg md:rounded lg:rounded border-2 border-light-blue text-light-blue'>Register With Facebook</button>
+                                <button className='w-full lg:px-10 py-2 lg:py-3 md:py-3 md:px-5 rounded-lg md:rounded lg:rounded border-2 border-bright-orange text-bright-orange'>Register With Google</button>
+                                <button className='w-full lg:px-10 py-2 lg:py-3 md:py-3 md:px-5 rounded-lg md:rounded lg:rounded border-2 border-light-blue text-light-blue'>Register With Facebook</button>
                             </div>
 
                             {/* ADD functionality to redirect to sign in page */}
