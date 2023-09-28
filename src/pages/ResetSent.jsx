b0e1692286bea847ed0667357375a13ede83436e
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-09-28 23:56:24 +0200

    :lipstick: Fixed image placement
    fixed image placement

--- src/pages/ResetSent.jsx
+++ src/pages/ResetSent.jsx
@@ -14,18 +14,26 @@ export default function ResetSent() {
             <div className="relative min-h-screen lg:overflow-hidden overflow-visible justify-center">
                 <div className="display flex flex-col bg-white justify-center w-full min-h-screen sm:flex-row flex-grow">
 
-                    <div className='display flex row justify-center lg:items-start md:items-start items-start bg-lilac w-full lg:w-1/12 sm:w-full flex-grow lg:rounded-border pt-4 pb-12 md:pb-0 lg:pb-0'>
+                    <div className='display flex row justify-center lg:items-start md:items-start items-start bg-lilac w-full lg:w-1/12 sm:w-full flex-grow lg:rounded-border  md:pb-0 lg:pb-0'>
                         <div className='display flex-col'>
-                           {/* ADD functionality to back button */}
                             <div className='display flex row justify-center lg:pb-10 md:pb-10'>
-                                {/* <img src={backArrow} className='pl-6 lg:pl-10 md:pl-10 mt-2 mb-2' onClick={() => window.history.back()} alt="back arrow" /> */}
-                                <img src={aluveLogo} alt="aluve logo text" className='w-1/2 lg:hidden md:hidden' />
                                 <img 
                                     src={confusedImg} 
                                     alt="confused image" 
                                     className='hidden lg:block md:block transform translate-x-1/2 lg:translate-x-1/3 md:translate-x-1/3'
                                     style={{ maxWidth: '110%', width: '700px' }}
                                 />
+
+                                {/*same image in mobile view */}
+                                <div className='display flex justify-center align-center w-3/4'>
+                                <img 
+                                    src={confusedImg} 
+                                    alt="confused image" 
+                                    className='block lg:hidden md:hidden transform translate-x-0 '
+                                    style={{ maxWidth: '110%', width: '700px' }}
+                                />
+                                </div>
+                                
                             </div>
                         </div>
                     </div>
