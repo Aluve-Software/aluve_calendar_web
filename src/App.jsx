7a35a963a7bcaa4b820a25db5eb6f987f07306ab
Kagiso Moepya <kagisomoepya19@gmail.com>
2023-09-28 08:30:27 +0200

    :white_check_mark: Additions: Functionality to be used with UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,53 @@
 // import { useState } from 'react'
+import React from 'react'
+import { BrowserRouter,Routes, Route} from 'react-router-dom'
+import { LoggedInPrivateRoute, NotLoggedInPrivateRoute } from './routes'
+import { useDispatch, useSelector } from 'react-redux'
 import ResetPassword from './pages/ResetPassword'
 import NewPassword from './pages/NewPassword'
 import SuccessfulReset from './pages/SuccessfulReset'
-import { BrowserRouter  } from 'react-router-dom'
-import { Routes, Route  } from 'react-router-dom'
-import './App.css'
+import Registration from './pages/Registration'
+import Landing from './pages/Landing'
+import { Pages } from './pages'
 
 function App() {
-  
+  const currentUser = useSelector(state => state.AuthReducer.currentUser)
+  const dispatch = useDispatch()
+
+  React.useEffect(() => {
+
+    async function onAuthStateChange() {
+      const response = await axios.get('/current-user')
+      const data = await response.data
+      const user = data.user
+      dispatch(AuthActions.onAuthStateChange(user))
+    }
+
+    // onAuthStateChange()
+
+  }, [currentUser])
+
+
   return (
     <>
-      <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Registration />} />
           <Route path="/reset" element={<ResetPassword />} />
           <Route path="/newpassword" element={<NewPassword />} />
           <Route path="/successreset" element={<SuccessfulReset />} />
-        {/* <SuccessfulReset /> */}
-        {/* <NewPassword /> */}
-        {/* <ResetPassword /> */}
+          {/* <SuccessfulReset /> */}
+          {/* <NewPassword /> */}
+          {/* <ResetPassword /> */}
+          
+          {/* TO BE USED ONCE UI IS COMPLETE */}
+          <Route element={<NotLoggedInPrivateRoute currentUser={currentUser} />}>
+            <Route exact path='/*' element={<Pages.Authentication />} />
+          </Route>
+          <Route element={<LoggedInPrivateRoute currentUser={currentUser} />}>
+            {/* <Route path='/*' element={<Pages.Dashboard />} /> */}
+          </Route>
+
         </Routes>
-      </BrowserRouter>
-      
     </>
   )
 }
