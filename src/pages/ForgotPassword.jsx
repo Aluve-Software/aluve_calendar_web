e04ff7526bf1249b9f75d1b607fb6656d7f3642f
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-09-29 00:04:36 +0200

    :lipstick: Fixed image placement

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,7 +1,4 @@
-// import { useState } from 'react';
-// import backArrow from '../assets/backArrow.svg';
 import aluveLogo from '../assets/Aluve_monochrome_black 1.svg';
-// import aluveLogoNoText from '../assets/aluveBlackNoText.svg'
 import confusedImg from '../assets/confusedImg.svg'
 import '../index.css';
 
@@ -16,17 +13,23 @@ export default function ForgotPassword() {
 
                     <div className='display flex row justify-center lg:items-start md:items-start items-start bg-lilac w-full lg:w-1/12 sm:w-full flex-grow lg:rounded-border pt-4 pb-12 md:pb-0 lg:pb-0'>
                         <div className='display flex-col'>
-                           {/* ADD functionality to back button */}
                             <div className='display flex row justify-center lg:pb-10 md:pb-10'>
-                                {/* <img src={backArrow} className='pl-6 lg:pl-10 md:pl-10 mt-2 mb-2' onClick={() => window.history.back()} alt="back arrow" /> */}
-                                <img src={aluveLogo} alt="aluve logo text" className='w-1/2 lg:hidden md:hidden' />
                                 <img 
                                     src={confusedImg} 
                                     alt="confused image" 
                                     className='hidden lg:block md:block transform translate-x-1/2 lg:translate-x-1/3 md:translate-x-1/3'
                                     style={{ maxWidth: '110%', width: '700px' }}
                                 />
-                            
+
+                                {/*same image in mobile view */}
+                                <div className='display flex justify-center align-center w-3/4'>
+                                <img 
+                                    src={confusedImg} 
+                                    alt="confused image" 
+                                    className='block lg:hidden md:hidden transform translate-x-0 '
+                                    style={{ maxWidth: '110%', width: '700px' }}
+                                />
+                                </div>
                             </div>
 
                         </div>
