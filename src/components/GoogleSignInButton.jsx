532c6e426f7c8134706eba788b1650ec0ba6155e
tshepoandi <lovekilledtshepo@gmail.com>
2023-10-02 14:13:39 +0200

    Google Registration

---
+++ src/components/GoogleSignInButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import { Link } from 'react-router-dom'
+
+function GoogleSignInButton() {
+  return (
+    <a href="/login/oauth2/authorization/google">
+       <button  className='w-full lg:px-10 py-2 lg:py-3 md:py-3 md:px-5 rounded-lg md:rounded lg:rounded border-2 border-bright-orange text-bright-orange'>Register With Google</button>
+    </a> 
+  )
+}
+
+export default GoogleSignInButton
