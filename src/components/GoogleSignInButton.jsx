ed28d8c8a46847f02baf23700500564e00c71ffe
tshepoandi <lovekilledtshepo@gmail.com>
2023-10-03 12:45:27 +0200

    google auth button styling

--- src/components/GoogleSignInButton.jsx
+++ src/components/GoogleSignInButton.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 function GoogleSignInButton() {
   return (
-    <a href="/oauth2/authorization/google">
-       <button className='w-full lg:px-10 py-2 lg:py-3 md:py-3 md:px-5 rounded-lg md:rounded lg:rounded border-2 border-bright-orange text-bright-orange flex items-center justify-center'> Register With Google</button>
-    </a> 
+         <a href="/oauth2/authorization/google" className='w-full lg:px-10 py-2 lg:py-3 md:py-3 md:px-5 rounded-lg md:rounded lg:rounded border-2 border-bright-orange text-bright-orange flex items-center justify-center'>
+          Register With Google
+         </a>
   )
 }
 
