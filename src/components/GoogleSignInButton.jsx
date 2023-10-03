8d20fe9f231f3155f2f2751c9687abb9af233899
tshepoandi <tshepomashiloane869@gmail.com>
2023-10-03 11:08:19 +0200

    google sign in to hwsr-1097014

--- src/components/GoogleSignInButton.jsx
+++ src/components/GoogleSignInButton.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 function GoogleSignInButton() {
+  const handleSignIn = () => {
+    // Redirect to the Google authorization URL
+    window.location.href = 'http://hwsrv-1097014.hostwindsdns.com:8080/oauth2/authorization/google';
+  };
+
   return (
-    <a href="/login/oauth2/authorization/google">
-       <button  className='w-full lg:px-10 py-2 lg:py-3 md:py-3 md:px-5 rounded-lg md:rounded lg:rounded border-2 border-bright-orange text-bright-orange flex flex-row'> Register With Google</button>
-    </a> 
-  )
+    <button
+      className='w-full lg:px-10 py-2 lg:py-3 md:py-3 md:px-5 rounded-lg md:rounded lg:rounded border-2 border-bright-orange text-bright-orange flex items-center justify-center'
+      onClick={handleSignIn}
+    >
+      Register With Google
+    </button>
+  );
 }
 
-export default GoogleSignInButton
+export default GoogleSignInButton;
+
+
+
 
