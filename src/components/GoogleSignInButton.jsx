8925dc85362b55c4d7a5cc2e81ff9c6363edff26
tshepoandi <tshepomashiloane869@gmail.com>
2023-10-03 11:10:16 +0200

    center-text

--- src/components/GoogleSignInButton.jsx
+++ src/components/GoogleSignInButton.jsx
@@ -1,23 +1,13 @@
-import React from 'react';
+import React from 'react'
+
 
 function GoogleSignInButton() {
-  const handleSignIn = () => {
-    // Redirect to the Google authorization URL
-    window.location.href = 'http://hwsrv-1097014.hostwindsdns.com:8080/oauth2/authorization/google';
-  };
-
   return (
-    <button
-      className='w-full lg:px-10 py-2 lg:py-3 md:py-3 md:px-5 rounded-lg md:rounded lg:rounded border-2 border-bright-orange text-bright-orange flex items-center justify-center'
-      onClick={handleSignIn}
-    >
-      Register With Google
-    </button>
-  );
+    <a href="/oauth2/authorization/google">
+       <button className='w-full lg:px-10 py-2 lg:py-3 md:py-3 md:px-5 rounded-lg md:rounded lg:rounded border-2 border-bright-orange text-bright-orange flex items-center justify-center'> Register With Google</button>
+    </a> 
+  )
 }
 
-export default GoogleSignInButton;
-
-
-
+export default GoogleSignInButton
 
